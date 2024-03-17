80c95b19704ea9ba098b8bac78a6656f32e9a867
Vivek Mathur <71319149+Grimoors@users.noreply.github.com>
2024-03-18 02:19:04 +0530

    Update Setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ setup(
     install_requires=[
           'pytest',
           'ganga',
-          'pickle',
-          "pdfminer",
+          "pdfminer.six",
           "PyPDF2",
           "shutil"
       ],
